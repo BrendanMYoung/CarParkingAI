87bf689d2d995e382c78d3044319f45ee1c36ce4
bmy3369 <bmy3369@rit.edu>
2022-12-05 18:52:07 -0500

    Finished representation of a car parking genetic algorithm

--- parking.py
+++ parking.py
@@ -1,11 +1,13 @@
 import numpy as np
 import sys
 import scipy
+import time
 import random
 import matplotlib.pyplot as plt
 import math
+import struct
 from scipy import interpolate
-population_size = 201
+population_size = 321
 mutation_rate = .005
 size_of_parameter_vector = 7
 max_generations = 1200
@@ -20,43 +22,28 @@ time_steps = 10
 steps_per_time_step = 0.1
 # List of the time steps to use for interpolation
 all_time_steps_for_x = np.arange(0, time_steps, steps_per_time_step)
-
 amt_control_variables = 2
 total_entries = time_steps * amt_control_variables
 # Creates a new chromosome for a single time step
 chromosomes = [0, 1]
+total_chromosomes = 10
 
 initial_state =  [0, 8, 0, 0]
+
 final_state = np.array([0, 0, 0, 0])
 K = 200
 cost_for_being_out_of_bounds = 1
 stop_criteria = 0.1
 
-def newChromeosome():
-    return np.random.choice(chromosomes, size_of_parameter_vector).tolist()
-def createNewChromeosome(val):
-    chromesome = [1, 0, 0, 0, 0, 0, 0]
-    return chromesome
+total_bits = size_of_parameter_vector * (total_entries - 2)  #subtract 2 for the initial 2 states
 
-# Creates a new sequence of chromosomes for an entire time step
-def newChromesomoneSequence():
-    chromeosomeSequence = []
-    # Set up first chromeosome to have the initial state as timestep = 0
-    chromeosomeSequence.insert(0, createNewChromeosome(initial_state[2]))
-    chromeosomeSequence.insert(1, createNewChromeosome(initial_state[3]))
-    # Generate random chromeosomes
-    for i in range(0, total_entries, 1):
-        chromeosomeSequence.append(newChromeosome())
-    return chromeosomeSequence
-# Generates the entire initial population percept sequences
-def generateNewRandomPopulation():
-    population = []
-    for i in range(0, population_size, 1):
-        population.append(newChromesomoneSequence())
+# Creates a new gray coded binary sequence
+def newChromosome():
+    chromosomeSequence = [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    chromosomeSequence += np.random.choice(chromosomes, total_bits).tolist()
+    return chromosomeSequence
 
-    return population
-
-# Converts the number from binary to decimal, the first index determines if the number is negative or positive
+# Take in an array of 7 values for a value sequence of a gene to convert it to decimal
 def binaryToDecimal(binary_array):
     decimal = 0
     for i in range(len(binary_array) -1, -1, -1):
@@ -64,18 +51,43 @@ def binaryToDecimal(binary_array):
     if binary_array[0] == 0:
         decimal *= -1
     return decimal
+
+def flip(c):
+    return 1 if(c == 0) else 0;
+def graytoBinary(gray):
+    binary = [];
+
+    binary.append(gray[0])
+
+    # Compute remaining bits
+    for i in range(1, len(gray)):
+
+        # If current bit is 0,
+        # concatenate previous bit
+        if (gray[i] == 0):
+            binary.append(binary[i - 1])
+
+        # Else, concatenate invert
+        # of previous bit
+        else:
+            binary.append(flip(binary[i - 1]))
+
+    return binary;
+
 # Converts a given binary input into standardized accelerations and headings
 # Convert from - 2^(size_of_parameter_vector) - 1, to [min, max]
 max = pow(2, size_of_parameter_vector)-1
 def convertHeading(binary_chromosome):
-    oldVal = binaryToDecimal(binary_chromosome)
+    #binary_chromosome = graytoBinary(graycode_chromosome.tolist())
+    oldVal = abs(binaryToDecimal(binary_chromosome))
     newRange = max_heading - min_heading
-    return (((oldVal - 0) * newRange ) / max) + min_heading
+    return ((oldVal * newRange ) / max) + min_heading
 # Converts acceleration
 def convertAcceleration(binary_chromosome):
-    oldVal = binaryToDecimal(binary_chromosome)
+    #binary_chromosome = graytoBinary(graycode_chromosome.tolist())
+    oldVal = abs(binaryToDecimal(binary_chromosome))
     newRange = max_acceleration - min_acceleration
-    return (((oldVal - 0) * newRange) / max) + min_acceleration
+    return ((oldVal * newRange) / max) + min_acceleration
 
 def costFunction(local_final_state, cf):
     if cf == 0:
@@ -83,8 +95,15 @@ def costFunction(local_final_state, cf):
     else:
         cost = K + cf
     return cost
+def generateNewPopulation():
+    population = []
+    for i in range(0, population_size, 1):
+        population.append(newChromosome())
+    return population
 
-def convertChromesomeSequence(chromesome):
+def convertChromesomeSequence(non_split_chromosome):
+    chromesome = np.array_split(non_split_chromosome, total_entries)
+    optimization_parameter = []
     acceleration_history = []
     heading_history = []
     x = []
@@ -94,23 +113,17 @@ def convertChromesomeSequence(chromesome):
         heading = convertHeading(chromesome[i])
         acceleration = convertAcceleration(chromesome[i+1])
 
-        acceleration_history.append(heading)
-        heading_history.append(acceleration)
-    time = np.linspace(0, time_steps, num=time_steps+1, endpoint = True)
-    #s = interpolate.splrep(time, acceleration_history, s=0)
-   # h = interpolate.splrep(time, heading_history, s=0)
-    #acceleration_new = interpolate.splev(all_time_steps_for_x, s, der=0)
+        acceleration_history.append(acceleration)
+        heading_history.append(heading)
+        optimization_parameter.append(heading)
+        optimization_parameter.append(acceleration)
 
-    #heading_new = interpolate.splev(all_time_steps_for_x, h, der=0)
+    time = np.linspace(0, time_steps, num=time_steps, endpoint = True)
     s = interpolate.CubicSpline(time, acceleration_history, bc_type='natural')
     h = interpolate.CubicSpline(time, heading_history, bc_type='natural')
     x_new = np.linspace(0, 10, 100)
     acceleration_new = s(x_new)
-
     heading_new = h(x_new)
-
-    #tck = interpolate.splrep(time, acceleration_history, s=0)
-    #print(tck)
 
     current_x = initial_state[0]
     current_y = initial_state[1]
@@ -119,14 +132,14 @@ def convertChromesomeSequence(chromesome):
     for timeStep in range(0, len(all_time_steps_for_x)):
         current_velocity = acceleration_new[timeStep]
         current_heading = heading_new[timeStep]
-        current_x += steps_per_time_step * current_heading * math.cos(current_velocity)
-        current_y += steps_per_time_step * current_heading * math.sin(current_velocity)
+        current_x += steps_per_time_step * current_velocity * math.cos(current_heading)
+        current_y += steps_per_time_step * current_velocity * math.sin(current_heading)
+
         if isNotInBounds(current_x, current_y):
             local_cost += math.pow(getYValue(current_x) - current_y, 2 ) * steps_per_time_step
         x.append( current_x)
         y.append( current_y)
     local_final_state = np.array([current_x, current_y, current_heading, current_velocity])
-
     cost = costFunction(local_final_state, local_cost)
     return {
         "cost": cost,
@@ -134,32 +147,30 @@ def convertChromesomeSequence(chromesome):
         "x": x,
         "y": y,
         "acceleration": acceleration_new,
-        "heading": heading_new
+        "heading": heading_new,
+        "optimization_vector": optimization_parameter
     }
     #return [cost, x, y]
 
 def crossOver(pop1, pop2):
     # For every chromsome in each population
     cross_over_pivot = np.random.randint(0, len(pop1))
-    new_child1 = pop2[0:cross_over_pivot] + pop1[cross_over_pivot:]
-    new_child2 = pop1[0:cross_over_pivot] + pop2[cross_over_pivot:]
+    second_pivot = np.random.randint(cross_over_pivot, len(pop1))
+    new_child1 = pop2[0:cross_over_pivot] + pop1[cross_over_pivot:second_pivot] + pop2[second_pivot:]
+    new_child2 = pop1[0:cross_over_pivot] + pop2[cross_over_pivot:second_pivot] + pop1[second_pivot:]
     new_c_1 = []
     new_c_2 = []
     # do mutation of children
     for i in range(len(new_child1)):
-        new_c_1.insert(i, [])
-        new_c_2.insert(i, [])
-
-        for j in range(len(new_child1[i])):
-            new_c_1[i].insert(j, random.choices(
-                [new_child1[i][j], (new_child1[i][j]+1 % 2)],
-                weights=[(1-mutation_rate), mutation_rate]
-            )[0]
-                              )
-            new_c_2[i].insert(j, random.choices(
-                [new_child2[i][j], (new_child2[i][j]+1 % 2)],
-                weights=[1-mutation_rate, mutation_rate]
-            )[0] )
+        new_c_1.insert(i, random.choices(
+            [new_child1[i], (new_child1[i] + 1 % 2)],
+            weights=[(1 - mutation_rate), mutation_rate]
+        )[0]
+                          )
+        new_c_2.insert(i, random.choices(
+            [new_child2[i], (new_child2[i] + 1 % 2)],
+            weights=[1 - mutation_rate, mutation_rate]
+        )[0])
     return [new_c_1, new_c_2]
 # Do crossover breeding
 def newPopulationCrossover(chance_array, pops):
@@ -191,26 +202,11 @@ def doPopulationGeneration(pops, generation):
         fitness_of_pops.insert(i, values)
         if values["fitness"] > fitness_of_pops[most_fit_pop_index]["fitness"]:
             most_fit_pop_index = i
-    print("Generation " +str(generation) +" : " + str(fitness_of_pops[most_fit_pop_index]["cost"]) +" / " + str(fitness_of_pops[most_fit_pop_index]["fitness"]))
+    print("Generation " +str(generation) +" : J = " + str(fitness_of_pops[most_fit_pop_index]["cost"]) )
 
 
     if fitness_of_pops[most_fit_pop_index]["fitness"] >= 1-stop_criteria or generation >= max_generations:
-        print('done')
-        figure, axis = plt.subplots(3, 2)
-        axis[0, 0].plot(fitness_of_pops[most_fit_pop_index]["x"], fitness_of_pops[most_fit_pop_index]["y"])
-        axis[0, 0].set_title("x/y history")
-        axis[0, 1].plot(all_time_steps_for_x, fitness_of_pops[most_fit_pop_index]["acceleration"])
-        axis[0, 1].set_title("acceleration")
-        axis[1, 0].plot(all_time_steps_for_x, fitness_of_pops[most_fit_pop_index]["heading"])
-        axis[1, 0].set_title("heading")
-        axis[2, 0].plot(all_time_steps_for_x, fitness_of_pops[most_fit_pop_index]["x"])
-        axis[2, 0].set_title("x")
-        axis[2, 1].plot(all_time_steps_for_x, fitness_of_pops[most_fit_pop_index]["y"])
-        axis[2, 1].set_title("y")
-
-
-        plt.show()
-        return None
+        return None, fitness_of_pops[most_fit_pop_index]
 
     # Calculate fitness chance and setup random selection for crossover with pops
     chance_array = []
@@ -221,7 +217,46 @@ def doPopulationGeneration(pops, generation):
     new_generation = newPopulationCrossover(chance_array, pops)
     #print(new_generation)
     new_generation.append(pops[most_fit_pop_index])
-    return new_generation
+    return new_generation, fitness_of_pops[most_fit_pop_index]
+
+def finalState(most_fit):
+    print("")
+    print("Final state values:")
+    print("x_f = " + str(most_fit["x"][-1]))
+    print("y_f = " + str(most_fit["y"][-1]))
+    print("alpha_f = " + str(most_fit["heading"][-1]))
+    print("v_f = " + str(most_fit["acceleration"][-1]))
+
+    figure, axis = plt.subplots(5, 1, figsize=(4, 2 * 5), tight_layout= True)
+
+    axis[0].set_xlim([-15, 15])
+    axis[0].set_ylim([-10, 15])
+    axis[0].plot([-15, -4], [3, 3], 'k-', lw=1)
+    axis[0].plot([-4, -4], [-1, 3], 'k-', lw=1)
+    axis[0].plot([-4, 4], [-1, -1], 'k-', lw=1)
+    axis[0].plot([4, 4], [-1, 3], 'k-', lw=1)
+    axis[0].plot([15, 4], [3, 3], 'k-', lw=1)
+    axis[0].plot(most_fit["x"], most_fit["y"])
+    axis[0].plot([-15, -4], [3, 3], 'k-', lw=1)
+    axis[0].plot([-4, -4], [-1, 3], 'k-', lw=1)
+    axis[0].plot([-4, 4], [-1, -1], 'k-', lw=1)
+    axis[0].plot([4, 4], [-1, 3], 'k-', lw=1)
+    axis[0].plot([15, 4], [3, 3], 'k-', lw=1)
+    plt.setp(axis[0], xlabel="x (ft)", ylabel="y (ft)")
+
+    axis[1].plot(all_time_steps_for_x, most_fit["acceleration"])
+    plt.setp(axis[1], xlabel="Time (s)", ylabel="beta (ft/s^2)")
+
+    axis[2].plot(all_time_steps_for_x, most_fit["heading"])
+    plt.setp(axis[2], xlabel="Time (s)", ylabel="alpha (theta/s^2)")
+
+    axis[3].plot(all_time_steps_for_x, most_fit["x"])
+    plt.setp(axis[3], xlabel="Time (s)", ylabel="x (ft)")
+
+    axis[4].plot(all_time_steps_for_x, most_fit["y"])
+    plt.setp(axis[4], xlabel="Time (s)", ylabel="y (ft)")
+
+    plt.show()
 
 def isNotInBounds(x, y):
     if x <= -4 and y > 3:
@@ -240,15 +275,20 @@ def getYValue(x):
     elif x >= 4:
         return 3
     return 0
+
 def main():
+    start_time = time.time()
     generations = 0
-    current_population = generateNewRandomPopulation()
-    most_fit = None
-    while (current_population != None):
-        current_population = doPopulationGeneration(current_population, generations)
-        generations +=1
+    current_population = generateNewPopulation()
+    while (current_population != None and time.time() -start_time < 420):
+        current_population, most_fit = doPopulationGeneration(current_population, generations)
+        generations += 1
 
-
+    finalState(most_fit)
+    file = open('control.dat', 'w')
+    for val in most_fit["optimization_vector"]:
+        file.write(str(val)+"\n")
+    file.close()
 
 if __name__ == "__main__":
     main()
